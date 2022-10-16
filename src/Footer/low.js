75eec2c9ee91057f163f51f19451e4096cfd26cf
Anwik Uwakov <anwik55@gmail.com>
2022-10-16 22:00:21 +0300

    homework timer

--- src/Footer/low.js
+++ src/Footer/low.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
@@ -7,16 +8,28 @@ import Countdown from 'react-countdown';
 function Low ({hourValue, setHourValue}) {
 
     const endOfDay = new Date().setHours(23,59,59,999);
+    const [showElement, setShowElement] = useState('countdown');
+    const [time, setTime] = useState(endOfDay);
 
     const cheapHours = [
         {label: '1h', value: '1'},
         {label: '2h', value: '2'},
         {label: '3h', value: '3'},
         {label: '4h', value: '4'},
-        {label: '6h', value: '5'},
-        {label: '8h', value: '6'},
+        {label: '6h', value: '6'},
+        {label: '8h', value: '8'},
     ];
+    
     function handleOnChange(event) {
+        const hour = event.currentTarget.value;
+        const newDate = new Date().setHours(23 - hour,59,59,999);
+        if(newDate - Date.now() <= 0) {
+            setShowElement('right now');
+        } else {
+            setShowElement('countdown');
+        }
+    //     // setShowElement('right now');
+        setTime(newDate);
         setHourValue(event.currentTarget.value);
     }
  return (   
@@ -46,7 +59,7 @@ function Low ({hourValue, setHourValue}) {
             </Row>
             <Row>
                 <Col>
-                    <Countdown date={endOfDay} />
+                   {showElement === 'countdown' ? <Countdown date={time} /> : <h3>Right Now!</h3>}
                 </Col>
             </Row>
             <Row>
