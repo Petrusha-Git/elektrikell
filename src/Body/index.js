01e01c8ce93e4dd19403817c4c8be99589f7ad64
Anwik Uwakov <anwik55@gmail.com>
2022-10-17 20:43:30 +0300

    api integration

--- src/Body/index.js
+++ src/Body/index.js
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import { getPriceData } from '../services/apiService';
+import ErrorModal from '../ErrorModal';
 
 const data = [
     {
@@ -49,7 +51,25 @@ const data = [
 ];
 
 function Body() {
+
+    const [showError, setShowError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    useEffect(() => {
+        (async function () {
+           try {
+            const response = await getPriceData();
+            console.log(response);
+           } catch (error) {
+                setShowError(true);
+                setErrorMessage(error.message);
+           }
+        }) ();
+
+    }, []);
+
     return (
+        <>
         <Row>
             <Col>
                 <ResponsiveContainer width="100%" height="100%" minHeight="500px">
@@ -75,6 +95,8 @@ function Body() {
                 </ResponsiveContainer>
             </Col>
         </Row>
+        <ErrorModal errorMessage={errorMessage} show={showError} setShow={setShowError} />   
+        </>
     );
 }
 
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,19 +1,39 @@
+import { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
+import { getCurrentPrice } from '../services/apiService';
+import ErrorModal from '../ErrorModal';
 
 function Header(props) {
+
+    const [price, setPrice] = useState(0);
+    const [showError, setShowError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    useEffect(() => {
+        (async function () {
+            try{
+                const response = await getCurrentPrice();
+                setPrice(response.data[0].price);
+            } catch(error) {
+                setShowError(true);
+                setErrorMessage(error.message);
+            }
+        }) ();
+
+    }, []);
+
 
     const radios = [
         { name: 'Low price', value: 'Low' },
         { name: 'High Price', value: 'High' },
     ];
-    
-function handleOnChange(event) {
-  // event.preventDefault();
-  props.setRadioValue(event.currentTarget.value);
-}
+
+    function handleOnChange(event) {
+        // event.preventDefault();
+        props.setRadioValue(event.currentTarget.value);
+    }
     return (
         <>
             <Row>
@@ -39,8 +59,9 @@ function handleOnChange(event) {
                         ))}
                     </ButtonGroup>
                 </Col>
-                <Col>HIND</Col>
+                <Col>HIND {price}eur /MWh</Col>
             </Row>
+            <ErrorModal errorMessage={errorMessage} show={showError} setShow={setShowError} />
         </>
     );
 }
