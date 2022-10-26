ff34d6df86f712f78158ec934bafde6852d800fb
Anwik Uwakov <anwik55@gmail.com>
2022-10-26 18:06:29 +0300

    setData

--- src/Body/index.js
+++ src/Body/index.js
@@ -38,7 +38,12 @@ function Body({
                         timestamp: dataObject.timestamp,
                     };
                 });
-                setData(priceData);
+                
+                if(!data) {
+                    setData(priceData);
+                    return;
+                };
+
 
                 const hourNowI = priceData.findIndex(dataObject => {
                     return dataObject.x === moment().format('HH');
